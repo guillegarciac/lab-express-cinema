a3fa9e686f0bce857ac4e7ffbaa20ef26c6f2bdf
guille garcia <ggarciacardiel@gmail.com>
2022-12-20 18:19:17 +0100

    iteration 1 - added seed  node ./seeds/movies.seed.js

--- seeds/movies.seed.js
+++ seeds/movies.seed.js
@@ -81,3 +81,26 @@ const movies = [
   }
 ];
 
+const mongoose = require ("mongoose");
+const Movie = require ("../models/Movie.model");
+const MONGODB_URI = 'mongodb://localhost:27017/lab-express-cinema';
+mongoose.set('strictQuery', true);
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(x => {
+    console.log(`Connected to the database: "${x.connection.name}"`);
+    // Before adding any movies to the database, let's remove all existing ones
+    return Movie.deleteMany();
+  })
+  .then(() => {
+    return Movie.create(movies);
+  })
+  .then(createdMovies => console.log(createdMovies))
+  .then(() => {
+    mongoose.disconnect();
+  })
+  .catch(error => {
+    console.error('Error connecting to the database', error);
+  });
+
