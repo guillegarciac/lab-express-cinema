332066bc4e1f931e863bd7905e0037337a9c706c
guille garcia <ggarciacardiel@gmail.com>
2022-12-22 16:36:39 +0100

    added search + not found

--- routes/movies.js
+++ routes/movies.js
@@ -12,6 +12,20 @@ router.get ("/", async (req,res,next) => {
   }
 })
 
+//Search always beofre GET one otherwise it will be ignored
+
+/* GET search results */
+/* ROUTE /movies/search */
+router.get('/search', async function (req, res, next) {
+  const { title } = req.query;
+  try {
+    const movie = await Movie.findOne({ title: title });
+    res.render('search', { query: title, movie: movie });
+  } catch (error) {
+    next(error)
+  }
+});
+
 /* GET one movie */
 /* ROUTE /movies/:movieId */
 router.get ("/:movieId", async (req,res,next) => {
@@ -23,5 +37,7 @@ router.get ("/:movieId", async (req,res,next) => {
     next(error);
   }
 })
+
+
 
 module.exports = router;
